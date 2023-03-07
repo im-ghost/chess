8400c598e46a0b3ea6641f759f093dc5073aabd5
Ghost <culestfrosh@gmail.com>
2023-03-07 22:59:21 +0100

    testing chess

--- src/App.js
+++ src/App.js
@@ -1,56 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import {useEffect,useState} from 'react';
 import './App.css';
-
+import { ChessBoard } from "react-chessboard";
+import { Chess } from "chessjs"
+import eruda from "eruda"
 function App() {
+  const [game,setGame] = useState(new Chess())
+  const saveGameMutate = (modify) =>{
+    return setGame((g)=>{
+      const update = {...g}
+      modify(update)
+      return update
+    })
+  }
+  const computerMovement = () =>{
+    const possibleMove = game.move();
+    if(game.is_over() || game.in_draw() || possibleMove.length < 0) return;
+    const randIndex = Math.floor(Math.random()*possibleMove.length)
+    saveGameMutate((game)=>{
+      game.move(possibleMove[randIndex])
+    })
+  }
+  const onDrop = (source, target) =>{
+    let move = null
+    move= game.move({
+      from:source,
+      to:target,
+      promotion:"q"
+    })
+    if(move === null) return false
+    setTimeout(function() {
+      computerMovement()
+    }, 200);
+    return true;
+  }
+  useEffect(()=>{
+    alert("hie");
+    eruda.init()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+     <ChessBoard
+       postion={game.fen()}
+       onDrop={onDrop}
+      />
     </div>
   );
 }
